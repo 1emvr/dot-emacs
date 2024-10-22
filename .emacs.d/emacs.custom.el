d9eb5bb8fb3d2ac36d0d4b9d950efa217c8e704f
1emvr <lemur.proxymslns@outlook.com>
2024-10-22 06:18:57 -0400

    updating emacs.custom.el

--- .emacs.d/emacs.custom.el
+++ .emacs.d/emacs.custom.el
@@ -1,5 +1,6 @@
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 (add-to-list 'default-frame-alist '(font . "AcPlus Rainbow100 re.66 14"))
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
 (add-to-list 'load-path "C:/Users/lemur/AppData/Roaming/.emacs.d/elpa")
 (add-to-list 'load-path "C:/Users/lemur/AppData/Roaming/.emacs.d/elpa/magit/lisp")
@@ -10,14 +11,16 @@
 (require 'magit)
 (require 'disable-mouse)
 (require 'simpc-mode)
+(require 'rust-mode)
 (require 'multiple-cursors)
-
-(add-to-list 'auto-mode-alist '("\\.[hc]\\(pp\\)?\\'" . simpc-mode))
 
 (with-eval-after-load 'info
   (info-initialize)
   (add-to-list 'Info-directory-list
 	       "C:/Users/lemur/AppData/Roaming/.emacs.d/elpa/magit/Documentation/"))
+
+
+(add-to-list 'auto-mode-alist '("\\.[hc]\\(pp\\)?\\'" . simpc-mode))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -27,26 +30,22 @@
  '(custom-enabled-themes '(gruber-darker))
  '(custom-safe-themes
    '("e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7" default))
- '(shell-file-name "powershell.exe")
  '(display-line-numbers-type 'relative)
  '(global-disable-mouse-mode nil)
  '(global-display-line-numbers-mode 1)
- 
  '(ido-everywhere t)
  '(ido-mode 'both nil (ido))
  '(ido-show-dot-for-dired t)
-  
+ '(menu-bar-mode nil)
  '(package-check-signature 'allow-unsigned)
  '(package-selected-packages
-   '(multiple-cursors webpaste wfnames with-editor transient-cycles gnu-elpa-keyring-update dash compat gruber-darker-theme))
-
+   '(rust-mode multiple-cursors webpaste wfnames with-editor transient-cycles gnu-elpa-keyring-update dash compat gruber-darker-theme))
+ '(shell-file-name "powershell.exe")
+ '(tab-width 4)
  '(tool-bar-mode nil)
- '(menu-bar-mode nil)
  '(truncate-lines t)
  '(truncate-partial-width-windows t)
- '(visible-bell t)
- '(tab-width 4)
-)
+ '(visible-bell t))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -54,4 +53,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
 
