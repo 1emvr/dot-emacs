fc37120ded101ef6acee17e872795b95f99b3ea8
1emvr <lemur.proxymslns@outlook.com>
2024-10-22 05:04:20 -0400

    updating emacs.custom.el

--- .emacs.d/emacs.custom.el
+++ .emacs.d/emacs.custom.el
@@ -1,16 +1,23 @@
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 (add-to-list 'default-frame-alist '(font . "AcPlus Rainbow100 re.66 14"))
 
+(add-to-list 'load-path "C:/Users/lemur/AppData/Roaming/.emacs.d/elpa")
 (add-to-list 'load-path "C:/Users/lemur/AppData/Roaming/.emacs.d/elpa/magit/lisp")
 (add-to-list 'load-path "C:/Users/lemur/AppData/Roaming/.emacs.d/elpa/disable-mouse")
+(add-to-list 'load-path "C:/Users/lemur/AppData/Roaming/.emacs.d/elpa/simpc-mode")
+(add-to-list 'load-path "C:/Users/lemur/AppData/Roaming/.emacs.d/elpa/multiple-cursors-1.4.0")
 
 (require 'magit)
 (require 'disable-mouse)
+(require 'simpc-mode)
+(require 'multiple-cursors)
+
+(add-to-list 'auto-mode-alist '("\\.[hc]\\(pp\\)?\\'" . simpc-mode))
 
 (with-eval-after-load 'info
   (info-initialize)
   (add-to-list 'Info-directory-list
-			   "C:/Users/lemur/AppData/Roaming/.emacs.d/elpa/magit/Documentation/"))
+	       "C:/Users/lemur/AppData/Roaming/.emacs.d/elpa/magit/Documentation/"))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -20,27 +27,25 @@
  '(custom-enabled-themes '(gruber-darker))
  '(custom-safe-themes
    '("e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7" default))
-
  '(shell-file-name "powershell.exe")
- '(package-check-signature 'allow-unsigned)
-
- '(package-selected-packages
-   '(webpaste wfnames with-editor transient-cycles gnu-elpa-keyring-update dash compat gruber-darker-theme))
-
+ '(display-line-numbers-type 'relative)
+ '(global-disable-mouse-mode nil)
+ '(global-display-line-numbers-mode 1)
+ 
  '(ido-everywhere t)
  '(ido-mode 'both nil (ido))
  '(ido-show-dot-for-dired t)
+  
+ '(package-check-signature 'allow-unsigned)
+ '(package-selected-packages
+   '(multiple-cursors webpaste wfnames with-editor transient-cycles gnu-elpa-keyring-update dash compat gruber-darker-theme))
 
- '(display-line-numbers-type 'relative)
- '(global-display-line-numbers-mode 1)
- '(global-disable-mouse-mode)
-
- '(tab-width 4)
  '(tool-bar-mode nil)
  '(menu-bar-mode nil)
  '(truncate-lines t)
  '(truncate-partial-width-windows t)
  '(visible-bell t)
+ '(tab-width 4)
 )
 
 (custom-set-faces
