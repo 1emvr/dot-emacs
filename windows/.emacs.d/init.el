8f612771831f2c4c833f7c34b0b6ff6095133db0
1emvr <1emvr@protonmail.com>
2024-11-23 09:18:52 -0500

    updating i3 config and adding fonts

--- windows/.emacs.d/init.el
+++ windows/.emacs.d/init.el
@@ -1,5 +1,5 @@
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
-(set-frame-font "AcPlus Rainbow100 re.66 14")
+(set-frame-font "IosevkaTerm NFM 12")
 
 (setq inhibit-startup-message t) 
 (setq initial-scratch-message nil)
@@ -24,29 +24,29 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(gruber-darker))
+ '(c++-basic-offset 4)
+ '(c-basic-offset 4)
+ '(custom-enabled-themes '(doom-material-dark))
  '(custom-safe-themes
-   '("e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7" default))
+   '("f64189544da6f16bab285747d04a92bd57c7e7813d8c24c30f382f087d460a33" "5f128efd37c6a87cd4ad8e8b7f2afaba425425524a68133ac0efd87291d05874" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "5b9a45080feaedc7820894ebbfe4f8251e13b66654ac4394cb416fef9fdca789" "00cec71d41047ebabeb310a325c365d5bc4b7fab0a681a2a108d32fb161b4006" "9d29a302302cce971d988eb51bd17c1d2be6cd68305710446f658958c0640f68" "2721b06afaf1769ef63f942bf3e977f208f517b187f2526f0e57c1bd4a000350" "ff24d14f5f7d355f47d53fd016565ed128bf3af30eb7ce8cae307ee4fe7f3fd0" "991ca4dbb23cab4f45c1463c187ac80de9e6a718edc8640003892a2523cb6259" "5586a5db9dadef93b6b6e72720205a4fa92fd60e4ccfd3a5fa389782eab2371b" "dc8285f7f4d86c0aebf1ea4b448842a6868553eded6f71d1de52f3dcbc960039" "631c52620e2953e744f2b56d102eae503017047fb43d65ce028e88ef5846ea3b" "016f665c0dd5f76f8404124482a0b13a573d17e92ff4eb36a66b409f4d1da410" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525" "bf948e3f55a8cd1f420373410911d0a50be5a04a8886cabe8d8e471ad8fdba8e" "512ce140ea9c1521ccaceaa0e73e2487e2d3826cc9d287275550b47c04072bc4" "6945dadc749ac5cbd47012cad836f92aea9ebec9f504d32fe89a956260773ca4" "7e377879cbd60c66b88e51fad480b3ab18d60847f31c435f15f5df18bdb18184" "adaf421037f4ae6725aa9f5654a2ed49e2cd2765f71e19a7d26a454491b486eb" "443e2c3c4dd44510f0ea8247b438e834188dc1c6fb80785d83ad3628eadf9294" "1a1ac598737d0fcdc4dfab3af3d6f46ab2d5048b8e72bc22f50271fd6d393a00" "da75eceab6bea9298e04ce5b4b07349f8c02da305734f7c0c8c6af7b5eaa9738" "1aa4243143f6c9f2a51ff173221f4fd23a1719f4194df6cef8878e75d349613d" "0c08a5c3c2a72e3ca806a29302ef942335292a80c2934c1123e8c732bb2ddd77" "545ab1a535c913c9214fe5b883046f02982c508815612234140240c129682a68" "2e05569868dc11a52b08926b4c1a27da77580daa9321773d92822f7a639956ce" "ce4234c32262924c1d2f43e6b61312634938777071f1129c7cde3ebd4a3028da" "7ea883b13485f175d3075c72fceab701b5bf76b2076f024da50dff4107d0db25" "8d3ef5ff6273f2a552152c7febc40eabca26bae05bd12bc85062e2dc224cde9a" "e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7" default))
  '(display-line-numbers-type 'relative)
  '(evil-mode 1)
  '(evil-want-C-u-scroll t)
  '(global-disable-mouse-mode nil)
  '(global-display-line-numbers-mode 1)
+ '(hl-block-global-mode t)
  '(ido-everywhere t)
  '(ido-mode 'both nil (ido))
  '(ido-show-dot-for-dired t)
  '(menu-bar-mode nil)
  '(package-selected-packages
-   '(hl-block-mode gruber-darker rust-mode multiple-cursors magit gruber-darker-theme fzf evil disable-mouse))
+   '(doom-themes ## hl-block-mode gruber-darker rust-mode multiple-cursors magit gruber-darker-theme fzf evil disable-mouse))
  '(shell-file-name "powershell.exe")
  '(tab-width 4)
  '(tool-bar-mode nil)
  '(truncate-lines t)
  '(truncate-partial-width-windows t)
- '(winner-mode 1)
- '(c-basic-offset 4)
- '(c++-basic-offset 4)
- '(hl-block-global-mode t))
+ '(winner-mode 1))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
